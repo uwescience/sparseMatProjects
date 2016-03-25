8e94dcc5cfda5843d8812f1f95c6a9b4e8f5c4c9
Brandon Myers <bdmyers@cs.washington.edu>
2016-03-25 15:25:41 -0700

    catalogs

---
+++ matrices/catalogs/README.md
@@ -0,0 +1,1 @@
+This directory contains the catalogs in python file format, which can be read by Raco.
---
+++ matrices/catalogs/catalog.py
@@ -0,0 +1,6 @@
+{ 
+   'public:adhoc:matrix': [('row', 'LONG_TYPE'),
+                           ('col', 'LONG_TYPE'),
+                           ('value', 'DOUBLE_TYPE'),
+                           ]
+   }
