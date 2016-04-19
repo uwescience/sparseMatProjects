10ac831208c131562b9fe52bb3fd0ad4d65bba73
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-19 14:24:37 -0700

    using '

--- myriaLQueries/radish/setup_ramdisk.sh
+++ myriaLQueries/radish/setup_ramdisk.sh
@@ -7,6 +7,6 @@ fi
 
 mkdir /mnt/ramdisk
 mount -t tmpfs -o size=40g tmpfs /mnt/ramdisk
-su - sgeadmin -c "ln -s /mnt/ramdisk $HOME/ramdisk"
-su - sgeadmin -c "mv $HOME/sparseMatrixProjects $HOME/ramdisk"
-su - sgeadmin -c "ln -s $HOME/ramdisk/sparseMatrixProjects"
+su - sgeadmin -c 'ln -s /mnt/ramdisk $HOME/ramdisk'
+su - sgeadmin -c 'mv $HOME/sparseMatProjects $HOME/ramdisk'
+su - sgeadmin -c 'ln -s $HOME/ramdisk/sparseMatProjects'
