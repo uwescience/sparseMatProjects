57f55b23d429b40837821c1373e764672c1873ac
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-19 15:41:33 -0700

    ingest reflects new s3 location

--- myriaLQueries/radish/ingest_dataset.sh
+++ myriaLQueries/radish/ingest_dataset.sh
@@ -1,15 +1,18 @@
+set -o nounset
+
 export $PATH=$PATH:$HOME/local/bin
 name=$1
+data=$HOME/data
 
 cd $HOME/raco/c_test_environment && \
-	python grappa_ingest.py -i $HOME/sparseMatProjects/matrices/$name \
-                                -c $HOME/sparseMatProjects/matrices/catalogs/catalog.py \
+	python grappa_ingest.py -i $data/$name \
+                -c $HOME/sparseMatProjects/matrices/catalogs/catalog.py \
 				-n graph \
-                        	-s grappa \
+                -s grappa \
 			  	--softlink-data \
 				--allow-failed-upload \
-                        	--storage binary \
+                --storage binary \
 				--delim ' '
 
-cd $HOME/grappa/build/Make+Release/applications/join && ln -s $HOME/sparseMatProjects/matrices/$name.bin
+cd $HOME/grappa/build/Make+Release/applications/join && ln -s $data/$name.bin
 
