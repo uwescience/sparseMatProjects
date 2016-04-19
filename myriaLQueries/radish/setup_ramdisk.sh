39aa42a3bdb413b1c50c0f7b9154a61aa33f5d5a
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-19 15:07:02 -0700

    make this safer

--- myriaLQueries/radish/setup_ramdisk.sh
+++ myriaLQueries/radish/setup_ramdisk.sh
@@ -6,7 +6,19 @@ if [ `whoami` != 'root' ]; then
 fi
 
 mkdir /mnt/ramdisk
-mount -t tmpfs -o size=40g tmpfs /mnt/ramdisk
-su - sgeadmin -c 'ln -s /mnt/ramdisk $HOME/ramdisk'
-su - sgeadmin -c 'mv $HOME/data $HOME/ramdisk'
-su - sgeadmin -c 'cd $HOME && ln -s ramdisk/data'
+mkdir -p $HOME/data
+
+if mount | grep ramdisk > /dev/null; then 
+    echo "SKIP: mount /mnt/ramdisk"
+then
+    echo "mount /mnt/ramdisk"
+    mount -t tmpfs -o size=40g tmpfs /mnt/ramdisk
+fi
+
+if [ -L $HOME/data ]; then
+    echo "SKIP: moving and linking data"   
+else
+    echo "moving and linking data"   
+    su - sgeadmin -c 'mv $HOME/data /mnt/ramdisk'
+    su - sgeadmin -c 'ln -s /mnt/ramdisk/data $HOME/data'
+fi
