7b0a19d1192b7e5a0c8c75bed51c05ae9fb22c9a
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-28 17:01:57 -0700

    runtime vs num tuples

--- myriaLQueries/radish/compare_runtimes.R
+++ myriaLQueries/radish/compare_runtimes.R
@@ -1,40 +1,15 @@
 library(ggplot2)
-library(sqldf)
 
+source("cleandata.R")
 
-raw <- read.csv("google_sheet.csv", header=TRUE, sep=',')
-colnames(raw) <- c("system", "task_string", "task_logical", "task_physical", "instance_type", "nworkers", "dataset", "runtime", "notes1", "notes2")
-
-cleaned <- na.omit(raw)
-
-# Resolve dataset names
-#resolved <- sqldf(c("update cleaned set dataset='undirNet_1000_sm.matrix.dat' where dataset='undirNet_1000.matrix.dat'",
-#                    "select * from main.cleaned"))
-#resolved <- sqldf(c("update resolved set dataset='undirNet_5000_sm.matrix.dat' where dataset='undirNet_5000.matrix.dat'",
-#                    "select * from main.resolved"))
-cleaned[cleaned$dataset=='undirNet_1000.matrix.dat',]$dataset <- 'undirNet_1000_sm.matrix.dat'
-cleaned[cleaned$dataset=='undirNet_5000.matrix.dat',]$dataset <- 'undirNet_5000_sm.matrix.dat'
-
-# Resolve application names
-resolved <- sqldf("select *, 'btwnCent' as task from cleaned
-                 where task_string='btwnCent' or task_string='btwnCent_batch_MyriaL'
-                 union all
-                 select *, 'MM' as task from cleaned
-                 where task_string='Matrix Multiply' or task_string='sparseMatMultQuery_MyriaL'
-                 or task_string='sparseMatMultQuery_MyriaL.sql'
-                 union all
-                 select *, '3MM' as task from cleaned
-                 where task_string='Three Mat Mult' or task_string='threeSparseMatMultQuery_MyriaL'
-                 or task_string='threeSparseMatMultQuery_MyriaL.sql'
-                 union all
-                 select *, 'MCL' as task from cleaned
-                 where task_string='MCL' or task_string='MCL_MyriaL' or task_string='MCL_MyriaL.sql'")
-stopifnot(dim(cleaned)[0]==dim(resolved)[0])
-
-plot <- ggplot(resolved, aes(x=dataset, y=runtime, fill=system)) +
+plot <- ggplot(data, aes(x=dataset, y=runtime, fill=system)) +
   stat_summary(fun.y="mean", geom="bar", position="dodge", aes(width=0.9)) + 
   facet_grid(task ~ .) +
   scale_y_continuous(trans='log2') +
+  xlab("Dataset") +
+  ylab("Time (s)") + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 ggsave(plot=plot, filename='compare_runtimes.pdf')
+
+
